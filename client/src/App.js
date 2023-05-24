787be81bac811180402bd98b6fba942498b38733
Vinod Pal <undervinod@gmail.com>
2023-05-24 16:54:21 +0530

    added navbar to the react

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import {useEffect} from 'react';
+import Navbar from './components/Navbar';
 
 function App() {
   useEffect(()=>{
@@ -9,22 +9,9 @@ function App() {
       .then((data)=>console.log(data));
   },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is health tracking application.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          EnergizeExpress
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
