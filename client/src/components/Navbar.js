18d7ad0538ef8068ad4d26bb981a21cce076859c
Vinod Pal <undervinod@gmail.com>
2023-05-26 13:29:49 +0530

    Added modified logo

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css'
 import { NavLink } from 'react-router-dom';
+import logo from '../images/logo.png';
 
 const Navbar = props => {
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="/">EnergizeExpress</a>
+       {/* <NavLink className="navbar-brand" to="/"><img src={logo} alt="logo" /></NavLink> */}
+       <NavLink className="navbar-brand" to="/"><b><i>EnergizeExpress</i></b></NavLink>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
