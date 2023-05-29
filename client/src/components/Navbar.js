3e05b9565091fb84120addc724472ace1342e7e4
Vinod Pal <undervinod@gmail.com>
2023-05-29 14:17:27 +0530

    removed unused object

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css'
 import { NavLink } from 'react-router-dom';
-import logo from '../images/logo.png';
 
 const Navbar = props => {
   return (
