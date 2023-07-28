c3cda4ec5ee29fa75c67952a456a5f941bbaab64
Vinod Pal <undervinod@gmail.com>
2023-07-28 19:16:02 +0530

    072023: Added login and registration

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,35 @@
-import './App.css';
-import {useEffect} from 'react';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import AboutUs from './components/Aboutus';
-import { Route,Routes } from 'react-router-dom';
-import ContactUs from './components/ContactUs';
-import WeightLog from './components/WeightLog';
-import WorkouttLog from './components/WorkoutLog';
-import Register from './components/Register';
-import WeeklyWorkoutProgram from './components/WeeklyWorkoutProgram';
+import "./App.css";
+import { useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import AboutUs from "./components/Aboutus";
+import { Route, Routes } from "react-router-dom";
+import ContactUs from "./components/ContactUs";
+import WeightLog from "./components/WeightLog";
+import WorkouttLog from "./components/WorkoutLog";
+import Register from "./components/Register";
+import WeeklyWorkoutProgram from "./components/WeeklyWorkoutProgram";
+import LoginForm from "./components/Login";
 
 function App() {
-  useEffect(()=>{
-      fetch('/test')
-      .then(res=>res.json())
-      .then((data)=>console.log(data));
-  },[])
+  useEffect(() => {
+    fetch("/test")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
   return (
     <>
-    <div className='menubar'>
-    <Navbar />
-    </div>
-     
+      <div className="menubar">
+        <Navbar />
+      </div>
       <Routes>
-      <Route path='/' Component={Home} />
-      <Route path='/register' Component={Register} />
-      <Route path='/aboutus' Component={AboutUs} />
-      <Route path='/contactus' Component={ContactUs} />
-      <Route path='/weight' Component={WeightLog} />
-      <Route path='/workout' Component={WorkouttLog} />
-      <Route path='/program' Component={WeeklyWorkoutProgram} />
+        <Route path="/" Component={LoginForm} />
+        <Route path="/register" Component={Register} />
+        <Route path="/aboutus" Component={AboutUs} />
+        <Route path="/contactus" Component={ContactUs} />
+        <Route path="/weight" Component={WeightLog} />
+        <Route path="/workout" Component={WorkouttLog} />
+        <Route path="/program" Component={WeeklyWorkoutProgram} />
       </Routes>
     </>
   );
