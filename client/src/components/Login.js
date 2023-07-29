8835815df53e64102438f5b953f05f64681ffd60
Vinod Pal <undervinod@gmail.com>
2023-07-29 18:01:47 +0530

    Added styling for login and registration page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 
 const LoginForm = () => {
+  const [isSignupMode, setIsSignupMode] = useState(false);
+
+  const toggleSignUpIn = () => {
+    setIsSignupMode(!isSignupMode);
+  };
+
   return (
-    <div className="login-container">
+    <div className={`login-container  ${isSignupMode ? "sign-up-mode" : ""}`}>
       <div className="forms-container">
         <div className="signin-signup">
           <form action="#" className="sign-in-form">
-            <h2 className="title">Sign in</h2>
+            <h2 className="title">Login</h2>
             <div className="input-field">
               <i className="fas fa-user"></i>
               <input type="text" placeholder="Username" />
@@ -17,7 +24,7 @@ const LoginForm = () => {
               <input type="password" placeholder="Password" />
             </div>
             <input type="submit" value="Login" className="btn solid" />
-            <p className="social-text">Or Sign in with social platforms</p>
+            <p className="social-text">Or Login with social platforms</p>
             <div className="social-media">
               <a href="#" className="social-icon">
                 <i className="fab fa-facebook-f"></i>
@@ -72,10 +79,14 @@ const LoginForm = () => {
           <div className="content">
             <h3>New here ?</h3>
             <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
-              ex ratione. Aliquid!
+              Get ready for an awesome ride – sign up now and let the fun begin
+              in no time! 🎉
             </p>
-            <button className="btn transparent" id="sign-up-btn">
+            <button
+              className="btn transparent"
+              onClick={toggleSignUpIn}
+              id="sign-up-btn"
+            >
               Sign up
             </button>
           </div>
@@ -89,11 +100,15 @@ const LoginForm = () => {
           <div className="content">
             <h3>One of us ?</h3>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-              laboriosam ad deleniti.
+              Login now to access your workout log and get started on the road
+              to fitness and well-being! 🏋️‍♂️
             </p>
-            <button className="btn transparent" id="sign-in-btn">
-              Sign in
+            <button
+              className="btn transparent"
+              onClick={toggleSignUpIn}
+              id="sign-in-btn"
+            >
+              Login
             </button>
           </div>
           <img
